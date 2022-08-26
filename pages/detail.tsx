98c0c0a08656973736292daaefe03ac901a75155
Hoppy0616 <hodahoda.20430@gmail.com>
2022-08-27 01:48:24 +0900

    詳細画面のからファイルの追加

---
+++ pages/detail.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import { css } from '@emotion/react'
+
+export const Detail: NextPage = () => {
+    return(
+        <div css={main}>
+
+        </div>
+    );
+
+}
+const main = css``;
